e311143408a0d91cf268829b6a8d912d2667c44a
Yicheng Wang <alex.wyc2098@protonmail.ch>
2018-09-03 00:21:45 -0400

    top'ed the nav bar

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
-    var navtop = $('nav').offset().top;
-    console.log(navtop);
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > navtop) {
-            $('nav').addClass('nfixed');
-            // $('nav').css('background-color', 'rgba(32, 79, 128, 1)');
-        }
-        else {
-            $('nav').removeClass('nfixed');
-            var alpha = $(window).scrollTop() / navtop;
-            // $('nav').css('background-color', 'rgba(32, 79, 128, '+ alpha + ')');
-        }
-    });
+    var nav_offset = $('nav').offset();
+    console.log($('nav').height());
+    console.log(nav_offset.top);
+    $("#space-holder").height($('nav').height());
+    //$(window).scroll(function() {
+    //    if ($(window).scrollTop() > navtop) {
+    //        $('nav').addClass('nfixed');
+    //        // $('nav').css('background-color', 'rgba(32, 79, 128, 1)');
+    //    }
+    //    else {
+    //        $('nav').removeClass('nfixed');
+    //        var alpha = $(window).scrollTop() / navtop;
+    //        // $('nav').css('background-color', 'rgba(32, 79, 128, '+ alpha + ')');
+    //    }
+    //});
 });
